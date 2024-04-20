f86c51c39a25f1a2bdc228986ea7456d7e0068f4
freakhuan <freakhuan@gmail.com>
2024-04-20 20:49:04 +0800

    U

--- Python/Guide.py
+++ Python/Guide.py
@@ -15,18 +15,30 @@ prompt = """
 """
 
 # 基于 prompt 生成文本
-# 默认使用 gpt-3.5-turbo 模型
 def get_completion(prompt, response_format="text", model=os.getenv('MY_MODEL')):
-    messages = [{"role": "user", "content": prompt}]    # 将 prompt 作为用户输入
+    """
+    获取模型完成的文本。
+
+    参数:
+    - prompt: 提供给模型的提示文本，作为用户输入。
+    - response_format: 希望接收的响应格式，默认为 "text"。可选值为 "text" 或 "json_object"。
+    - model: 使用的模型名称，默认从环境变量 'MY_MODEL' 获取。
+
+    返回值:
+    - 模型根据 prompt 生成的文本。
+    """
+    # 构造用户消息
+    messages = [{"role": "user", "content": prompt}]
+    # 向模型发送请求，获取响应
     response = client.chat.completions.create(
         model=model,
         messages=messages,
-        temperature=0,                                  # 模型输出的随机性，0 表示随机性最小
-        # 返回消息的格式，text 或 json_object
+        temperature=0,  # 设置随机性为最小
+        # 设置返回消息的格式
         response_format={"type": response_format},
     )
-    return response.choices[0].message.content          # 返回模型生成的文本
-
+    # 从响应中提取并返回生成的文本
+    return response.choices[0].message.content
 
 if __name__ == '__main__':
     # 交互模式 “提问：”，输入后回车
@@ -37,7 +49,7 @@ if __name__ == '__main__':
         if text == "退出":
             break
         prompt = prompt+"User:"+text
-        response = get_completion(prompt)
+        response = get_completion(prompt).replace("Assistant：", "")
         print("回答：", response)
         prompt = prompt+"\n" + "Assistant：" + response + "\n"
         # print(prompt)
